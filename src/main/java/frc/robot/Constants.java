8340bf2f171b660d60d10fcf335211fa0f08eaca
CorpuzIhate <guyihate@gmail.com>
2025-03-17 13:31:27 -0400

    added an import to constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -5,6 +5,7 @@
 package frc.robot;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.math.util.Units;
 
 /**
