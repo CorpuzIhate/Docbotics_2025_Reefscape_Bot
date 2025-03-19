a64a1bd3e8a96abda86594adfb4d238178bb229d
CorpuzIhate <guyihate@gmail.com>
2025-03-18 23:53:42 -0400

    commented and cleaned up robot container.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -48,205 +48,207 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class RobotContainer {
 
-
-  // configures the different subsystem of the robot
-  public final SwerveSub swerveSub =  new SwerveSub();
+  // configures the different subsystem of the robot,
+  public final SwerveSub swerveSub = new SwerveSub();
   public final CoralPitcherIntakeSub coralPitcherIntakeSub = new CoralPitcherIntakeSub();
   private final CoralIntakeConsumerSub coralIntakeConsumerSub = new CoralIntakeConsumerSub();
-  // private final ArmSub armsub = new ArmSub();
   private final LimelightSub limelightSub = new LimelightSub();
   public final ElevatorSub elevatorSub = new ElevatorSub();
   private final DismountSub dismountSub = new DismountSub();
   private final DismountSpinSub dismountSpinSub = new DismountSpinSub();
 
-
   private final SendableChooser<Command> autoChooser;
   private final Joystick driverJoyStick = new Joystick(OIConstants.kDriverControllerPort);
-  
+  /* D-pad Triggers. */
+  private final Trigger isDpadUpPressed = new Trigger(() -> {
+    return driverJoyStick.getPOV() == 0;
+  });
+  private final Trigger isDpadRightPressed = new Trigger(() -> {
+    return driverJoyStick.getPOV() == 90;
+  });
+  private final Trigger isDpadLeftPressed = new Trigger(() -> {
+    return driverJoyStick.getPOV() == 270;
+  });
 
   public RobotContainer() {
 
-    
-
-
-    Command swerveTest = new SwerveJoystickCmd(swerveSub, 
-    () -> -driverJoyStick.getRawAxis(OIConstants.kDriverYAxis),
-    () -> driverJoyStick.getRawAxis(OIConstants.kDriverXAxis),
-    () -> driverJoyStick.getRawAxis(OIConstants.kDriverRotAxis),
-    () -> driverJoyStick.getRawButtonPressed(OIConstants.kSlowModeIdx),
-    null,
-    null);
-    // Configure the trigger bindings
     swerveSub.setDefaultCommand(
-      new SwerveJoystickCmd(
-        swerveSub,
-        () -> -driverJoyStick.getRawAxis(OIConstants.kDriverYAxis),
-        () -> driverJoyStick.getRawAxis(OIConstants.kDriverXAxis),
-        () -> driverJoyStick.getRawAxis(OIConstants.kDriverRotAxis),
-        () -> driverJoyStick.getRawButtonPressed(OIConstants.kSlowModeIdx),
-         /// By default will be on field oriented.
-        () -> !
-        driverJoyStick.getRawButton(OIConstants.kDriverFieldOrientedButtonIdx),
-        () -> driverJoyStick.getRawButton(OIConstants.kOrientToTargetIdx)));
-
-
-        /* 
+        new SwerveJoystickCmd(
             swerveSub,
             () -> -driverJoyStick.getRawAxis(OIConstants.kDriverYAxis),
             () -> driverJoyStick.getRawAxis(OIConstants.kDriverXAxis),
             () -> driverJoyStick.getRawAxis(OIConstants.kDriverRotAxis),
             () -> driverJoyStick.getRawButtonPressed(OIConstants.kSlowModeIdx),
-             /// By default will be on field oriented.
-            () -> !
-            driverJoyStick.getRawButton(OIConstants.kDriverFieldOrientedButtonIdx), 
-            () -> driverJoyStick.getRawButton(OIConstants.kOrientToTargetIdx))); 
-             */
+            /// By default will be on field oriented.
+            () -> !driverJoyStick.getRawButton(OIConstants.kDriverFieldOrientedButtonIdx),
+            () -> driverJoyStick.getRawButton(OIConstants.kOrientToTargetIdx)));
+
     limelightSub.setDefaultCommand(
         new ManageLimeLightCMD(limelightSub));
 
     dismountSub.setDefaultCommand(
-      new MoveDismountArmCMD(dismountSub)
-    );
+        new MoveDismountArmCMD(dismountSub));
 
-    
     /**
      * By default the the elevator will be in Idle state
      * where it just tries to maintain the intake height set point.
      */
-     elevatorSub.setDefaultCommand(
-     new IdleIntakeHeightCMD(elevatorSub));
+    elevatorSub.setDefaultCommand(
+        new IdleIntakeHeightCMD(elevatorSub));
 
-     
-     coralPitcherIntakeSub.setDefaultCommand(
-      new IdlePitchIntakeAngleCMD(coralPitcherIntakeSub)
-     );
-     
-     coralIntakeConsumerSub.setDefaultCommand(
-      new powerCoralIntakeCMD(
-        coralIntakeConsumerSub, 
-        () -> driverJoyStick.getRawAxis(OIConstants.kIntakeAxis),
-        () -> driverJoyStick.getRawAxis(OIConstants.kOutakeAxis) ));
+    coralPitcherIntakeSub.setDefaultCommand(
+        new IdlePitchIntakeAngleCMD(coralPitcherIntakeSub));
+
+    coralIntakeConsumerSub.setDefaultCommand(
+        new powerCoralIntakeCMD(
+            coralIntakeConsumerSub,
+            () -> driverJoyStick.getRawAxis(OIConstants.kIntakeAxis),
+            () -> driverJoyStick.getRawAxis(OIConstants.kOuttakeAxis)));
 
     configureBindings();
-
-      autoChooser = AutoBuilder.buildAutoChooser(); // Default auto will be `Commands.none()`
+    /* add PathPlanner autos as options in auto selector. */
+    SmartDashboard.putData("Center_1Coral_F2_Reef", new PathPlannerAuto("Center_1Coral_F2_Reef"));
+    SmartDashboard.putData("Center_1Coral_I2_CoralStation", new PathPlannerAuto("Center_1Coral_I2_CoralStation"));
+    SmartDashboard.putData("Center_1Coral_I2_Reef", new PathPlannerAuto("Center_1Coral_I2_Reef"));
+    SmartDashboard.putData("MoveForward", new PathPlannerAuto("MoveForward"));
+    autoChooser = AutoBuilder.buildAutoChooser(); // Default auto will be `Commands.none()`
     SmartDashboard.putData("Auto Mode", autoChooser);
   }
 
-/**  Configure the trigger bindings for certain commands*/
+  /**
+   * Configure the trigger bindings for certain commands
+   * and registers them in PathPlanner
+   */
   private void configureBindings() {
-    // new JoystickButton(driverJoyStick, OIConstants.kMoveArmIdx ).whileTrue(new
-    // MoveArmCMD(armsub));
 
-
-  
-
-    /**Command to reset intake elevator motors */
-    Command resetEncodersCommand = new RunCommand(() -> {
+    /** Command to resets elevator encoders */
+    Command resetElevatorEncoderCommand = new RunCommand(() -> {
       elevatorSub.resetElevatorEncoders();
     }, elevatorSub);
-    SmartDashboard.putData("resetEncodersCommand", resetEncodersCommand);
 
+    /*
+     * creates a button on SmartDashboard
+     * for resetting the Encoders.
+     */
+    SmartDashboard.putData("resetEncodersCommand", resetElevatorEncoderCommand);
 
+    /** Command to get intake to height and angular coral station set-point. */
     Command consumerCoralAtCoralStation = new ParallelCommandGroup(
-      new InstantCommand(() -> 
-      {elevatorSub.setIntakeHeightSetPoint_Inches(30.8); 
-      coralPitcherIntakeSub.setIntakePitchSetpoint_degrees(144);}));
-      NamedCommands.registerCommand("consumeCoralAtCoralStation", consumerCoralAtCoralStation); 
+        new InstantCommand(() -> {
+          elevatorSub.setIntakeHeightSetPoint_Inches(30.8);
+          coralPitcherIntakeSub.setIntakePitchSetpoint_degrees(144);
+        }));
 
+    NamedCommands.registerCommand("consumeCoralAtCoralStation", consumerCoralAtCoralStation);
+
+    /* binds Command to trigger. */
+    new JoystickButton(driverJoyStick, OIConstants.kMoveIntakeToCoralStationIdx).onTrue(consumerCoralAtCoralStation);
+
+    /** Command to get intake to height and angular level 2 reef set-point. */
     Command scoreL2Reef = new ParallelCommandGroup(
-      new InstantCommand(() -> 
-      {elevatorSub.setIntakeHeightSetPoint_Inches(43);
-      coralPitcherIntakeSub.setIntakePitchSetpoint_degrees(91);}));
-      NamedCommands.registerCommand("scoreL2Reef", scoreL2Reef);
+        new InstantCommand(() -> {
+          elevatorSub.setIntakeHeightSetPoint_Inches(43);
+          coralPitcherIntakeSub.setIntakePitchSetpoint_degrees(91);
+        }));
 
+    /* registers the command in PathPlanner. */
+    NamedCommands.registerCommand("scoreL2Reef", scoreL2Reef);
+
+    /** binds Command to trigger. */
+    new JoystickButton(driverJoyStick, OIConstants.kMoveIntakeToLevel2Idx).onTrue(scoreL2Reef);
+
+    /** Command to get intake to height and angular level 3 reef set-point. */
     Command scoreL3Reef = new ParallelCommandGroup(
-        new InstantCommand(() -> 
-        {elevatorSub.setIntakeHeightSetPoint_Inches(84);
-        coralPitcherIntakeSub.setIntakePitchSetpoint_degrees(118);})); 
-        NamedCommands.registerCommand("scoreL3Reef", scoreL3Reef);
+        new InstantCommand(() -> {
+          elevatorSub.setIntakeHeightSetPoint_Inches(84);
+          coralPitcherIntakeSub.setIntakePitchSetpoint_degrees(118);
+        }));
+    /* registers the command in PathPlanner. */
 
+    NamedCommands.registerCommand("scoreL3Reef", scoreL3Reef);
+    /** binds Command to trigger. */
+    new JoystickButton(driverJoyStick, OIConstants.kMoveIntakeToLevel3Idx).onTrue(scoreL3Reef);
+
+    /** Command to get intake to height and angular default set-point. */
     Command setIntakePositionToDefault = new ParallelCommandGroup(
-      new InstantCommand(() -> 
-      {elevatorSub.setIntakeHeightSetPoint_Inches(0);
-      coralPitcherIntakeSub.setIntakePitchSetpoint_degrees(60);}));  
+        new InstantCommand(() -> {
+          elevatorSub.setIntakeHeightSetPoint_Inches(0);
+          coralPitcherIntakeSub.setIntakePitchSetpoint_degrees(60);
+        }));
+    /* registers the command in PathPlanner. */
+    NamedCommands.registerCommand("setIntakePositionToDefault", setIntakePositionToDefault);
 
-      Command scoreL1Reef  = new ParallelCommandGroup(
-        new InstantCommand(() -> 
-        {elevatorSub.setIntakeHeightSetPoint_Inches(0);
-        coralPitcherIntakeSub.setIntakePitchSetpoint_degrees(100);}));  
+    /** binds Command to trigger. */
+    new JoystickButton(driverJoyStick, OIConstants.kMoveIntakeToDefaultPosIdx).onTrue(setIntakePositionToDefault);
 
-      NamedCommands.registerCommand("setIntakePositionToDefault", setIntakePositionToDefault);
-      NamedCommands.registerCommand("intake", new autoPowerCoralIntakeCMD(coralIntakeConsumerSub, -0.3).withTimeout(3));
-      
+    /** Command to get intake to height and angular level 1 reef set-point. */
+    Command scoreL1Reef = new ParallelCommandGroup(
+        new InstantCommand(() -> {
+          elevatorSub.setIntakeHeightSetPoint_Inches(0);
+          coralPitcherIntakeSub.setIntakePitchSetpoint_degrees(100);
+        }));
 
-      NamedCommands.registerCommand("outtake", new autoPowerCoralIntakeCMD(coralIntakeConsumerSub, 0.3).withTimeout(0.5));
-      NamedCommands.registerCommand("resetSwerveModuleSpeedsCMD", new resetSwerveModuleSpeedsCMD(swerveSub));
-      new JoystickButton(driverJoyStick, OIConstants.kMoveIntakeToDefaultPosIdx).
-    onTrue(setIntakePositionToDefault);
-    /**When button pressed moved Intake to reef level 2 height and angle. */  
-    new JoystickButton(driverJoyStick, OIConstants.kMoveIntakeToLevel2Idx).
-    onTrue(scoreL2Reef);
-    
-    new JoystickButton(driverJoyStick, OIConstants.kMoveIntakeToLevel3Idx).
-    onTrue(scoreL3Reef);
-    new JoystickButton(driverJoyStick, OIConstants.kMoveIntakeToCoralStationIdx).
-    onTrue(consumerCoralAtCoralStation);
+    NamedCommands.registerCommand("intake", new autoPowerCoralIntakeCMD(coralIntakeConsumerSub, -0.3).withTimeout(3));
+    NamedCommands.registerCommand("outtake", new autoPowerCoralIntakeCMD(coralIntakeConsumerSub, 0.3).withTimeout(0.5));
+    NamedCommands.registerCommand("resetSwerveModuleSpeedsCMD", new resetSwerveModuleSpeedsCMD(swerveSub));
 
-    /**When button pressed reset the gyro. */
+    /** When button pressed reset the gyro. */
     new JoystickButton(driverJoyStick, OIConstants.kDriveGyroResetButtonIdx).whileTrue(
-      new ResetHeadingCMD(swerveSub)
-    );
+        new ResetHeadingCMD(swerveSub));
 
-    /**Command that dismounts Algea from the reef. */
-    Command dismountAlgeaL2CMD = 
-    new SequentialCommandGroup(
-      new InstantCommand(() -> { dismountSub.setIsHoldPosition(!dismountSub.getIsHoldPosition());}),
-      new ConditionalCommand(
+    /** Command that dismounts algae from reef level 2. */
+    Command dismountAlgaeL2CMD = new SequentialCommandGroup(
         new InstantCommand(() -> {
-          dismountSub.setSetpoint(DismountConstants.dismountAlegeSetpointL2_degrees);
-          dismountSpinSub.getDismountSpinMotor().set(0.6);}),
+          dismountSub.setIsHoldPosition(!dismountSub.getIsHoldPosition());
+        }),
+        new ConditionalCommand(
+            new InstantCommand(() -> {
+              dismountSub.setSetpoint(DismountConstants.dismountAlgaeSetPointL2_degrees);
+              dismountSpinSub.getDismountSpinMotor().set(0.6);
+            }),
+            new InstantCommand(() -> {
+              dismountSub.setSetpoint(0);
+              dismountSpinSub.getDismountSpinMotor().set(0);
+            }),
+            () -> {
+              return dismountSub.getIsHoldPosition();
+            }));
+    /** binds Command to trigger. */
+    isDpadLeftPressed.onTrue(dismountAlgaeL2CMD);
+    /** Command that dismounts Algae from reef level 3. */
+
+    Command dismountAlgaeL3CMD = new SequentialCommandGroup(
         new InstantCommand(() -> {
-          dismountSub.setSetpoint(0);
-          dismountSpinSub.getDismountSpinMotor().set(0);}), 
-        ()-> { return dismountSub.getIsHoldPosition();})
-    );
-    Command dismountAlgeaL3CMD = 
-    new SequentialCommandGroup(
-      new InstantCommand(() -> { dismountSub.setIsHoldPosition(!dismountSub.getIsHoldPosition());}),
-      new ConditionalCommand(
-        new InstantCommand(() -> {
-          dismountSub.setSetpoint(DismountConstants.dismountAlegeSetpointL3_degrees);
-          dismountSpinSub.getDismountSpinMotor().set(0.6);}),
-        new InstantCommand(() -> {
-          dismountSub.setSetpoint(0);
-          dismountSpinSub.getDismountSpinMotor().set(0);}), 
-        ()-> { return dismountSub.getIsHoldPosition();})
-    );
-    /*checks whether up on the d-pad is pressed. */
-    Trigger isDpadUpPressed = new Trigger(() -> {return driverJoyStick.getPOV() == 0;});
-    Trigger isDpadRightPressed = new Trigger(() -> {return driverJoyStick.getPOV() == 90;});
-    Trigger isDpadLeftPressed = new Trigger(() -> {return driverJoyStick.getPOV() == 270;});
+          dismountSub.setIsHoldPosition(!dismountSub.getIsHoldPosition());
+        }),
+        new ConditionalCommand(
+            new InstantCommand(() -> {
+              dismountSub.setSetpoint(DismountConstants.dismountAlgaeSetPointL3_degrees);
+              dismountSpinSub.getDismountSpinMotor().set(0.6);
+            }),
+            new InstantCommand(() -> {
+              dismountSub.setSetpoint(0);
+              dismountSpinSub.getDismountSpinMotor().set(0);
+            }),
+            () -> {
+              return dismountSub.getIsHoldPosition();
+            }));
+    /** binds Command to trigger. */
 
-    /**dismount Algea when up on the d-pad is pressed. */
+    isDpadUpPressed.onTrue(dismountAlgaeL3CMD);
 
-
-    isDpadUpPressed.onTrue(dismountAlgeaL3CMD);
-
-    isDpadLeftPressed.onTrue(dismountAlgeaL2CMD);
-
-    SmartDashboard.putData("Center_1Coral_F2_Reef" ,new PathPlannerAuto("Center_1Coral_F2_Reef"));
-    SmartDashboard.putData("Center_1Coral_I2_CoralStation" ,new PathPlannerAuto("Center_1Coral_I2_CoralStation"));
-
-    SmartDashboard.putData("Center_1Coral_I2_Reef" ,new PathPlannerAuto("Center_1Coral_I2_Reef"));
-
-    SmartDashboard.putData("MoveForward" ,new PathPlannerAuto("MoveForward"));
-    
   }
-  //** gets the autonmous Command for the robot */
+
+  /* registers commands into PathPlanner. */
+  public void configureNamedCommands() {
+
+  }
+
+  // ** gets the autonomous Command for the robot */
   public Command getAutonomousCommand() {
 
     return autoChooser.getSelected();
 
   }
+
 }
