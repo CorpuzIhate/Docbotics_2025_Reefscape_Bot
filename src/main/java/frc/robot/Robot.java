66c4e9060af77514f70de28e8b3035bb1a5b2aca
CorpuzIhate <guyihate@gmail.com>
2025-03-24 13:05:43 -0400

    added Usb Camera to Robot

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -60,7 +60,7 @@ public class Robot extends LoggedRobot {
 
     // Logger.disableDeterministicTimestamps() // See "Deterministic Timestamps" in the "Understanding Data Flow" page
     Logger.start(); // Start logging! No more data receivers, replay sources, or metadata values may be added.
-
+    CameraServer.startAutomaticCapture();
 
 
 
